37ade032d55e1bae6321936fb50438888493249d
squid-protocol <joe.m.esquibel@gmail.com>
2026-06-04 16:37:30 -0400

    feat(routing): map about and faq seo pages via templateview

--- math_app/urls.py
+++ math_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path("", views.index_view, name="index"),
     path("analytics/", views.analytics_view, name="analytics"),
     path("utility/", views.utility_dashboard, name="utility_dashboard"),
+    path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
+    path("faq/", TemplateView.as_view(template_name="faq.html"), name="faq"),
 ]
