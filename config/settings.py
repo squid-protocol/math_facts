1cfed76ec734ca24a449da928b47b51192d6adf4
squid-protocol <joe.m.esquibel@gmail.com>
2026-06-09 15:14:45 -0400

    style(config): apply black auto-formatting to settings

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,11 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "vue_client" / "dist", BASE_DIR / "frontend", BASE_DIR / "templates"],
+        "DIRS": [
+            BASE_DIR / "vue_client" / "dist",
+            BASE_DIR / "frontend",
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
